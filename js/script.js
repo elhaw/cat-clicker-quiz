c7e592dc2324948ea87352b96b0ec0ba943a3429
Computer <Computer>
2017-07-25 14:46:49 +0200

    add event listenr to track image clicks

--- js/script.js
+++ js/script.js
@@ -6,34 +6,34 @@ var cats = [{
         path: "img/cat.jpg",
         id: "0",
         name: 'lazy Cat',
-        clickVal :0
+        click :0
     },
     {
         path: "img/cat.jpg",
         id: "1",
         name: 'shy Cat',
-       clickVal :0
+       click :0
 
     },
      {
         path: "img/cat.jpg",
         id: "2",
         name: 'mmm Cat',
-        clickVal :0
+        click :0
 
     },
     {
         path: "img/cat.jpg",
         id: "3",
         name: 'beauty Cat',
-        clickVal :0
+        click :0
 
     },
     {
         path: "img/cat.jpg",
         id: "4",
         name: 'IT Cat',
-        clickVal :0
+        click:0
 
     }   
  ]
@@ -48,8 +48,17 @@ for (var i = 0; i < cats.length ; i ++)
 	var imageId = "cat"+i;
 	$('.left-container').append('<div id ="'+containerId+'">');
 	$('#'+containerId+'').append('<p>'+catName+'</p>');	
-	$('#'+containerId+'').append('<p id = "'+clickId+'">'+i+'</p>');
+	$('#'+containerId+'').append('<p id = "'+clickId+'">'+cats[i].click+'</p>');
 	$('#'+containerId+'').append('<img id ="'+imageId+'"src ="'+imgPath+'">');
 
+
+	$( '#cat'+i+'' ).click( (function(icopy) {
+	  return function() {
+	    cats[icopy].click++;
+	    $('#click'+icopy+'').text(cats[icopy].click);
+	  }
+	})(i));
 	
 }
+
+
