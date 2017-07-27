38822aafe7e397c108b4ad83c47c994ab05d3468
Computer <Computer>
2017-07-27 07:02:53 +0200

    reformat model

--- js/script.js
+++ js/script.js
@@ -1,39 +1,58 @@
+	var model = {
+
+			cats: [
+					{
+				        path: "img/cat_picture1.jpg",
+				        name: 'lazy Cat',
+				        click :0
+				    },
+				    {
+				        path: "img/cat_picture2.jpeg",
+				        name: 'shy Cat',
+				       click :0
+
+				    },
+				    {
+				        path: "img/cat_picture3.jpeg",
+				        name: 'cute Cat',
+				        click :0
+				    },
+				    {
+				        path: "img/cat_picture4.jpeg",
+				        name: 'wild Cat',
+				       click :0
+
+				    },
+				    {
+				        path: "img/cat_picture5.jpeg",
+				        name: 'smily Cat',
+				       click :0
+
+				    }
+		    ]
+	 
+	 };
 
 
-
-
-	var cats = [{
-	        path: "img/cat.jpg",
-	        name: 'lazy Cat',
-	        click :0
-	    },
-	    {
-	        path: "img/cat1.jpg",
-	        name: 'shy Cat',
-	       click :0
-
-	    }
-	 ]
-
-
-	for (var i = 0; i < cats.length ; i ++)
+	for (var i = 0; i < model.cats.length ; i ++)
 	{	
+
 		var containerId = "cat-container"+ i;
-		var catName = cats[i].name;
-		var imgPath = cats[i].path;
+		var catName = model.cats[i].name;
+		var imgPath = model.cats[i].path;
 		var clickId = "click"+i;
 		var imageId = "cat"+i;
 		$('#catlist').append('<button id = "button'+i+'"> cat'+i+' </button>')
 		$('.left-container').append('<div id ="'+containerId+'">');
 		$('#'+containerId+'').append('<p>'+catName+'</p>');	
-		$('#'+containerId+'').append('<p id = "'+clickId+'">'+cats[i].click+'</p>');
+		$('#'+containerId+'').append('<p id = "'+clickId+'">'+model.cats[i].click+' clicks</p>');
 		$('#'+containerId+'').append('<img id ="'+imageId+'"src ="'+imgPath+'">');
 
 
 		$( '#cat'+i+'' ).click( (function(icopy) {
 		  return function() {
-		    cats[icopy].click++;
-		    $('#click'+icopy+'').text(cats[icopy].click);
+		    model.cats[icopy].click++;
+		    $('#click'+icopy+'').text(model.cats[icopy].click +" clicks");
 		  }
 		})(i));
 		
@@ -47,7 +66,7 @@
 	}
 
 	function hideAllCats() {
-		for (var i = 0 ; i < cats.length ; i++)
+		for (var i = 0 ; i < model.cats.length ; i++)
 		{
 			$('#cat-container'+i+'').hide();
 		}
